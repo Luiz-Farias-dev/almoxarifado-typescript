97ed4cb76702e7a7f175fcccceb49ddd6cf7d1cc
Luiz-Farias-dev <luizferreirafariasn@gmail.com>
2026-01-14 16:18:06 -0300

    comecei a construir o middleware de signup

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,8 +2,17 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import User from "../models/user.model";
+import { gerarSenhaBase } from "../utils/signupHelpers";
 
-export const signUp = async (req: Request, res: Response, next: NextFunction) => {
+export const signUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { nome, cpf, tipoFuncionario } = req.body;
-    
+    const senha = gerarSenhaBase();
+  } catch (error) {
+    next(error);
+  }
+};
